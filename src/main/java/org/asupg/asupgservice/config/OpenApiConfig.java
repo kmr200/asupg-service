9a20be274c86010ff669cf637af7c9a42ce5e70b
Kamron <shuhikjuriknarik@gmail.com>
2026-02-26 19:20:16 +0500

    update version

--- src/main/java/org/asupg/asupgservice/config/OpenApiConfig.java
+++ src/main/java/org/asupg/asupgservice/config/OpenApiConfig.java
@@ -16,7 +16,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("ASUPG Service API")
-                        .version("1.0.0")
+                        .version("1.0.4")
                         .description("Backend API for ASUPG service"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
